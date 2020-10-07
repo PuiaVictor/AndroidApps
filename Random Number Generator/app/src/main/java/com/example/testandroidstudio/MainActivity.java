e384f791cefd4074ce29f72b7ec189d4f02c768f
unknown <puia_victor@yahoo.com>
2020-10-07 16:03:24 +0300

    Random Number Generator with number interval set from SeekBar

---
+++ Random Number Generator/app/src/main/java/com/example/testandroidstudio/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.testandroidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button generateButton = findViewById(R.id.generateButton);
+        final TextView resultTextView = findViewById(R.id.resultView);
+        final SeekBar seekBar = findViewById(R.id.seekBar);
+
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int rand = new Random().nextInt(seekBar.getProgress());
+                resultTextView.setText(String.valueOf(rand));
+            }
+        });
+    }
+}
